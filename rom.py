668c4253c7d739d1090dba6b9d58508aae90cd3b
Ignis Umbrae <ignisumbraevg@gmail.com>
2024-09-07 00:45:16 +0900

    Better way to patch in Up-Warp

--- rom.py
+++ rom.py
@@ -70,8 +70,8 @@ def write_tokens(world: "TJEWorld", patch: TJEProcedurePatch) -> None:
 
     # Add Upwarp Present if desired
     if world.options.upwarp_present:
-        patch.write_token(APTokenTypes.WRITE, 0x00010b0c, b"\x30\x03\x48\xC0\xD0\xBC\x00\x00\x00\x01\x2F\x00\x2F\x0A")
-        patch.write_token(APTokenTypes.WRITE, 0x00017be6, b"\x4e\x71\x4e\x71")
-        patch.write_token(APTokenTypes.WRITE, 0x000abc34, b"\x15\x10\x22\x17\x01\x12\x10")
+        patch.write_token(APTokenTypes.WRITE, 0x00010b06, b"\x10\x3C\x00\x19\x4E\x71") # Always up unless on level 25
+        patch.write_token(APTokenTypes.WRITE, 0x00017be6, b"\x4e\x71\x4e\x71") # Always show "item here" hint signs
+        patch.write_token(APTokenTypes.WRITE, 0x000abc34, b"\x15\x10\x22\x17\x01\x12\x10") # Change name to Up-Warp
 
     patch.write_file("token_data.bin", patch.get_token_binary())
