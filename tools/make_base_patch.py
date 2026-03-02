7c54a8f658f6592904449c216ebf8cc434947664
Ignis Umbrae <ignisumbraevg@gmail.com>
2026-03-03 03:52:37 +0900

    Use patch_util as module import

--- tools/make_base_patch.py
+++ tools/make_base_patch.py
@@ -1,39 +1,20 @@
-from pathlib import Path
-from enum import IntEnum, auto
 import copy
+from pathlib import Path
 
 import bsdiff4
 import pyjson5
 
-SPRITE_PATH = Path("../data/sprites_bin")
-CODE_PATH = Path("../data/asm_bin")
+from patch_util import extract_patches, BinType, set_bin_paths
 
-class BinType(IntEnum):
-    SPRITE = auto()
-    CODE = auto()
+set_bin_paths(Path("../data/asm_bin"), Path("../data/sprites_bin"))
 
-def read_bin(filename: Path, type: BinType) -> bytes:
-    match type:
-        case BinType.SPRITE:
-            filename = (SPRITE_PATH / filename).with_suffix(".bin")
-        case BinType.CODE:
-            filename = (CODE_PATH / filename).with_suffix(".bin")
-    with filename.open("rb") as file:
-        return file.read()
-
-def extract_patches(parsed_json5: str, type: BinType):
-    return tuple(
-        (addr, read_bin(patch.get("filename"), type))
-        for patch in parsed_json5.get("patches")
-        for addr in patch.get("addresses")
-    )
-
-with open("./asm/code_patches.json5", "r") as f:
+# collect and assemble base patches
+with open("./asm/base_code_patches.json5", "r") as f:
     code_patches = pyjson5.decode(f.read())
-with open("./asm/sprite_patches.json5", "r") as f:
+with open("./asm/base_sprite_patches.json5", "r") as f:
     sprite_patches = pyjson5.decode(f.read())
 
-static_rom_patches = extract_patches(code_patches, BinType.CODE) + extract_patches(sprite_patches, BinType.SPRITE)
+static_rom_patches = extract_patches(code_patches, BinType.ASM) + extract_patches(sprite_patches, BinType.SPRITE)
 
 with open("./rom_data/TJEREV02-orig.bin", "rb") as f:
     original_rom = bytearray(f.read())
@@ -45,3 +26,6 @@ for addr, val in static_rom_patches:
 diff = bsdiff4.diff(bytes(original_rom), bytes(patched_rom))
 with open("../data/base_patch.bsdiff4", "wb") as f:
     f.write(diff)
+
+# create json-formatted optional patches for use in rom.py
+
